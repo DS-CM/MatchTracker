fe8b7681affb74b5b76b28cac5923deb30747382
David Soller <soller.23@osu.edu>
2016-02-10 13:37:44 -0500

    Added basic App
    Default generated app

---
+++ app/src/main/java/group8/matchtracker/TournamentListFragment.java
@@ -0,0 +1,22 @@
+package group8.matchtracker;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class TournamentListFragment extends Fragment {
+
+    public TournamentListFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tournament_list, container, false);
+    }
+}
