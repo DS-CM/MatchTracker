62fd069cc998d04c3b2cbcb30b02d7468ed203c1
Chris McManamon <mcmanamon.14@buckeyemail.osu.edu>
2016-02-17 23:53:12 -0500

    Added a list for tournaments

--- app/src/main/java/group8/matchtracker/TournamentListFragment.java
+++ app/src/main/java/group8/matchtracker/TournamentListFragment.java
@@ -1,15 +1,16 @@
 package group8.matchtracker;
 
-import android.support.v4.app.Fragment;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+        import android.support.v4.app.ListFragment;
+        import android.os.Bundle;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.ArrayAdapter;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class TournamentListFragment extends Fragment {
+public class TournamentListFragment extends ListFragment {
 
     public TournamentListFragment() {
     }
@@ -17,6 +18,12 @@ public class TournamentListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_tournament_list, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_tournament_list, container, false);
+
+        String[] tournaments = new String[] {"Tournament 1\ndetails","Tournament 2\ndetails","Tournament 3\ndetails"};
+        ArrayAdapter adapter = new ArrayAdapter(getActivity(),android.R.layout.simple_list_item_1, tournaments);
+        setListAdapter(adapter);
+
+        return rootView;
     }
 }
