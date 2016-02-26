b894309ba6cb9f476553ff06dba12520b71194b0
Chris McManamon <mcmanamon.14@buckeyemail.osu.edu>
2016-02-26 18:39:01 -0500

    More complete version of Login

--- app/src/main/java/group8/matchtracker/activities/Login.java
+++ app/src/main/java/group8/matchtracker/activities/Login.java
@@ -1,16 +1,26 @@
 package group8.matchtracker.activities;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.view.View;
 
-public class Login extends FragmentActivity implements View.OnClickListener {
+import group8.matchtracker.R;
+
+public class Login extends FragmentActivity {
 
     public void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
-    }
+        setContentView(R.layout.login);
 
-    public void onClick(View v){
+        LoginFragment loginFragment = new LoginFragment();
+
+        FragmentManager fragmentManager = getFragmentManager();
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.add(R.id.logindetails,loginFragment);
+        fragmentTransaction.commit();
 
     }
 
--- app/src/main/java/group8/matchtracker/activities/LoginFragment.java
+++ app/src/main/java/group8/matchtracker/activities/LoginFragment.java
@@ -2,18 +2,27 @@ package group8.matchtracker.activities;
 
 
 import android.app.Fragment;
+import android.app.ListFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
-public class LoginFragment extends Fragment implements View.OnClickListener {
+import group8.matchtracker.R;
+
+public class LoginFragment extends ListFragment implements View.OnClickListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View v = inflater.inflate(R.layout.login_fragment, container, false);
 
+        String[] names = new String[] {"Name1", "Name2", "Name3", "Name4"};
+        ArrayAdapter adapter = new ArrayAdapter(getActivity(),android.R.layout.simple_list_item_1, names);
+        setListAdapter(adapter);
 
+        return v;
     }
 
     public void onClick(View v){
