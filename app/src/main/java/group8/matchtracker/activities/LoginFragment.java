6357aa378574e4e71a412f44e638069be7f4a9bb
Chris McManamon <mcmanamon.14@buckeyemail.osu.edu>
2016-02-26 20:51:30 -0500

    messed with listView

--- app/src/main/java/group8/matchtracker/activities/LoginFragment.java
+++ app/src/main/java/group8/matchtracker/activities/LoginFragment.java
@@ -5,27 +5,47 @@ import android.app.Fragment;
 import android.app.ListFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.util.Log;
 
 import group8.matchtracker.R;
 
 public class LoginFragment extends ListFragment implements View.OnClickListener {
 
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.login_fragment, container, false);
+        ListView lv = (ListView)v.findViewById(android.R.id.list);
 
         String[] names = new String[] {"Name1", "Name2", "Name3", "Name4"};
         ArrayAdapter adapter = new ArrayAdapter(getActivity(),android.R.layout.simple_list_item_1, names);
         setListAdapter(adapter);
 
+
         return v;
     }
 
+    @Override
     public void onClick(View v){
+
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id){
 
     }
 
