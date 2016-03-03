d6b9996ac00c0dc98d5ccdea45f8d0e6a9e966df
David Soller <soller.23@osu.edu>
2016-03-02 23:07:14 -0500

    Basic data displays in recyclerView

--- app/src/main/java/group8/matchtracker/activities/TournamentListFragment.java
+++ app/src/main/java/group8/matchtracker/activities/TournamentListFragment.java
@@ -1,19 +1,25 @@
 package group8.matchtracker.activities;
 
-import android.content.Context;
-import android.net.Uri;
+import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 import group8.matchtracker.R;
+import group8.matchtracker.adapters.TournamentAdapter;
+import group8.matchtracker.data.Tournament;
 
 
 public class TournamentListFragment extends Fragment {
+    private TournamentAdapter mTournamentAdapter;
+    private ArrayList<Tournament> mTournaments;
+
     public TournamentListFragment() {
         // Required empty public constructor
     }
@@ -21,15 +27,19 @@ public class TournamentListFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        RecyclerView recyclerView = (RecyclerView) this.getActivity().findViewById(R.id.tournamentlist_recycler_view);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this.getContext());
-        recyclerView.setLayoutManager(layoutManager);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.tournament_list_fragment, container, false);
+        View v =  inflater.inflate(R.layout.tournament_list_fragment, container, false);
+
+        RecyclerView recyclerView = (RecyclerView) v.findViewById(R.id.tournamentlist_recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(v.getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        mTournamentAdapter = new TournamentAdapter(v.getContext(), mTournaments);
+        recyclerView.setAdapter(mTournamentAdapter);
+
+        return v;
     }
 }
