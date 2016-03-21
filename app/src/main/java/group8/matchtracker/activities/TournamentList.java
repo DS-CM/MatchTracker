32e92c025d13cddc91502eba8011fcab3ff9bb6f
Chris McManamon <mcmanamon.14@buckeyemail.osu.edu>
2016-03-21 19:54:20 -0400

    uncommented api stuff

--- app/src/main/java/group8/matchtracker/activities/TournamentList.java
+++ app/src/main/java/group8/matchtracker/activities/TournamentList.java
@@ -45,31 +45,6 @@ public class TournamentList extends AppCompatActivity {
         Log.d(TAG, "onCreate");
     }
 
-    /**MENU STUFF FOR TESTING PURPOSES*/
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.menu_feed_activity, menu);
-
-        Log.d(TAG, "onCreateOptionsMenu");
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        switch(id){
-            case R.id.directions:
-                Intent i = new Intent(this,MapActivity.class);
-                startActivity(i);
-                break;
-            case R.id.action_settings:
-                break;
-        }
-
-        Log.d(TAG, "onOptionsItemSelected");
-        return super.onOptionsItemSelected(item);
-    }
-
     @Override
     protected void onStart() {
         super.onStart();
--- app/src/main/java/group8/matchtracker/activities/TournamentListFragment.java
+++ app/src/main/java/group8/matchtracker/activities/TournamentListFragment.java
@@ -9,10 +9,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 import group8.matchtracker.R;
 import group8.matchtracker.adapters.TournamentListAdapter;
+import group8.matchtracker.async.RetrieveTournamentsTask;
 import group8.matchtracker.data.Tournament;
 import group8.matchtracker.database.DatabaseHelper;
 
@@ -43,28 +48,28 @@ public class TournamentListFragment extends Fragment {
         LinearLayoutManager layoutManager = new LinearLayoutManager(v.getContext());
         mRecyclerView.setLayoutManager(layoutManager);
 
-//        RetrieveTournamentsTask rt = new RetrieveTournamentsTask();
-//        rt.setJsonDownloadListener(new RetrieveTournamentsTask.JsonDownloadListener() {
-//            @Override
-//            public void jsonDownloadedSuccessfully(JSONArray jsonArray) {
-//                try {
-//                    for (int i = 0; i < jsonArray.length(); i++) {
-//                        JSONObject object = jsonArray.getJSONObject(i);
-//                        mTournaments.add(mDbHelper.mTournamentTable.createTournament(object.getJSONObject("tournament").getString("name"), 0, 0, "Ohio Union", "esi"));
-//                        populateList(v);
-//                    }
-//                } catch (JSONException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        });
-//        rt.execute();
+        RetrieveTournamentsTask rt = new RetrieveTournamentsTask();
+        rt.setJsonDownloadListener(new RetrieveTournamentsTask.JsonDownloadListener() {
+            @Override
+            public void jsonDownloadedSuccessfully(JSONArray jsonArray) {
+                try {
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject object = jsonArray.getJSONObject(i);
+                        mTournaments.add(mDbHelper.mTournamentTable.createTournament(object.getJSONObject("tournament").getString("name"), 0, 0, "Ohio Union", "esi"));
+                        populateList(v);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        rt.execute();
 
         // TODO - Remove
         mDbHelper = new DatabaseHelper(v.getContext());
-        mDbHelper.mTournamentTable.createTournament("Shuffle VIII", 03122016, 03132016, "Ohio Union", "eSports Initiative");
+/*        mDbHelper.mTournamentTable.createTournament("Shuffle VIII", 03122016, 03132016, "Ohio Union", "eSports Initiative");
         mDbHelper.mTournamentTable.createTournament("Big House", 05032016, 05042016, "U of M", "The school up north");
-        mDbHelper.mTournamentTable.createTournament("EVO", 22, 23, "Cali", "EVO LLC");
+        mDbHelper.mTournamentTable.createTournament("EVO", 22, 23, "Cali", "EVO LLC");*/
 
         mTournaments = mDbHelper.mTournamentTable.getAllTournaments();
         populateList(v);
